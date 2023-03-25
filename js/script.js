3831a2438f74b845595e877c429746d8d9f08776
Renan Pansini <renanpansini15@gmail.com>
2023-03-25 19:19:24 -0400

    fetch bitcoin refatorado

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js";
-import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+import fetchBitcoin from "./modules/fetch-bitcoin.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -25,9 +25,9 @@ const tooltip = new Tooltip("[data-tooltip]");
 tooltip.init();
 
 initAnimacaoScroll();
-
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
+
 fetchAnimais('../../animaisapi.json', '.numeros-grid');
-initFetchBitcoin();
+fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
