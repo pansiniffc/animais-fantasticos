ad92d25cc3a9883e7a94dcf6227c212ef183835d
Renan Pansini <renanpansini15@gmail.com>
2023-03-26 16:20:30 -0400

    funcionamento refatorado

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ import Modal from "./modules/modal.js";
 import Tooltip from "./modules/tooltip.js";
 import DropdownMenu from "./modules/dropdown-menu.js";
 import MenuMobile from "./modules/menu-mobile.js";
-import initFuncionamento from "./modules/funcionamento.js";
+import Funcionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js";
 import fetchBitcoin from "./modules/fetch-bitcoin.js";
 
@@ -14,11 +14,15 @@ scrollSuave.init();
 
 const tabnav = new TabNav(
   "[data-tab='menu'] li",
-  "[data-tab='content'] section",
+  "[data-tab='content'] section"
 );
 tabnav.init();
 
-const modal = new Modal("[data-modal='abrir']", "[data-modal='fechar']", "[data-modal='container']");
+const modal = new Modal(
+  "[data-modal='abrir']",
+  "[data-modal='fechar']",
+  "[data-modal='container']"
+);
 modal.init();
 
 const tooltip = new Tooltip("[data-tooltip]");
@@ -32,7 +36,9 @@ dropdownMenu.init();
 
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 menuMobile.init();
-initFuncionamento();
 
-fetchAnimais('../../animaisapi.json', '.numeros-grid');
-fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
+const funcionamento = new Funcionamento("[data-semana]", "aberto");
+funcionamento.init();
+
+fetchAnimais("../../animaisapi.json", ".numeros-grid");
+fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
